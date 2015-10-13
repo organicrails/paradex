524252ba9b08b8b7469f52e6cd514b243cf6672f
Jack Hou <organicrails@gmail.com>
2015-10-13 18:22:39 -0400

    Add Show Method

--- app/controllers/api/paradoxes_controller.rb
+++ app/controllers/api/paradoxes_controller.rb
@@ -1,7 +1,12 @@
 class Api::ParadoxesController < ApplicationController
-	
+
 	# this will return all the Paradoxes in JSON format
 	def index
 		render json: Paradox.all
+	end
+
+	def show
+		paradox = Paradox.find(params[:id])
+		render json: paradox
 	end
 end
